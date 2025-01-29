2f919ec04e50c1b4208d4fafa89d6ec47ea66e43
Kornsupadej Suvarnakasa <k.suvarnakasa@gmail.com>
2025-01-29 21:26:30 +0700

    🎉 Initial commit

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import chalk from "chalk";
+import { globSync } from "glob";
+import merge from "deepmerge";
+import path from "path";
+
+const isArray = (item: any) => Array.isArray(item);
+
+const isLiteralObject = (item: any) =>
+  typeof item === "object" && !Array.isArray(item) && item !== null;
+
+const mergeConfig = (defs: any, envs: any, ...rest: any) =>
+  merge(defs, envs, ...rest);
+
+const extractConfig = (conf: any) => {
+  if (typeof conf !== "object") return conf;
+  return Object.keys(conf)
+    .map((v: any) => {
+      const envs = /^@(.*)/.exec(v);
+      const cenvs = /^@(.*?):([^:]*)/.exec(v);
+      const value: any = isLiteralObject(conf[v])
+        ? extractConfig(conf[v])
+        : isArray(conf[v])
+        ? conf[v].map((cf) => extractConfig(cf))
+        : conf[v];
+      if (cenvs && cenvs[1] && cenvs[2]) {
+        return { [cenvs[1]]: process.env[cenvs[2].toUpperCase()] || value };
+      } else if (envs && envs[1]) {
+        return { [envs[1]]: process.env[envs[1].toUpperCase()] || value };
+      } else {
+        return { [v]: value };
+      }
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    }, {});
+};
+
+const readDefaultConfig = import(
+  path.join(process.cwd(), `configs/default.ts`)
+);
+
+const readDevelopmentConfig = import(
+  path.join(process.cwd(), `configs/development.ts`)
+);
+
+const readProductionConfig = import(
+  path.join(process.cwd(), `configs/production.ts`)
+);
+
+const readConfigs = async () => {
+  const defs = await readDefaultConfig.then(({ default: conf }) => conf);
+  const envs =
+    process.env["NODE_ENV"] === "production"
+      ? await readProductionConfig.then(({ default: conf }) => conf)
+      : await readDevelopmentConfig.then(({ default: conf }) => conf);
+  return { defs, envs };
+};
+
+const validateEnv = () => {
+  const files = globSync(`./configs/${process.env["NODE_ENV"]}.ts`);
+  if (!files.length) {
+    if (process.env["NODE_ENV"]) {
+      console.error(
+        chalk.red(
+          '+ Error: No configuration file found for "' +
+            process.env["NODE_ENV"] +
+            '" environment, using development instead'
+        )
+      );
+    } else {
+      console.error(
+        chalk.red(
+          `+ Error: NODE_ENV is not defined. Using default development environment`
+        )
+      );
+    }
+    process.env["NODE_ENV"] = "development";
+  }
+};
+
+export const initGlobalConfig = async () => {
+  validateEnv();
+  const { defs, envs } = await readConfigs();
+  return extractConfig(mergeConfig(defs, envs));
+};
